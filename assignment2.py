40641607eb3e9a90aa9a0d2e29cbe236712c4026
gamejoon <gamejoon0325@naver.com>
2023-09-15 18:04:11 +0900

    Update assignment2.py
    turtle.onkey는 인자가 없는 함수만 사용이 가능하다.

--- assignment2.py
+++ assignment2.py
@@ -1,16 +1,32 @@
 import turtle
 
-def move_turtle(theta):
-    turtle.setheading(theta)
+def move_up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def move_down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def move_right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def move_left():
+    turtle.setheading(180)
     turtle.forward(50)
     turtle.stamp()
 
 turtle.shape("turtle")
 
-turtle.onkey(move_turtle(90), "w")
-turtle.onkey(move_turtle(270), "s")
-turtle.onkey(move_turtle(0), "d")
-turtle.onkey(move_turtle(180), "a")
+# turtle.onkey는 인자가 없는 함수만 사용 가능
+turtle.onkey(move_up, "w")
+turtle.onkey(move_down, "s")
+turtle.onkey(move_right, "d")
+turtle.onkey(move_left, "a")
 turtle.listen()
 
 turtle.exitonclick()
