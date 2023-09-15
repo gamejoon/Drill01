e92d019fd7ab8709eb6117744bb6f502adaf2989
gamejoon <gamejoon0325@naver.com>
2023-09-15 17:57:09 +0900

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,16 @@
 import turtle
+
+def move_turtle(theta):
+    turtle.setheading(theta)
+    turtle.forward(50)
+    turtle.stamp()
+
+turtle.shape("turtle")
+
+turtle.onkey(move_turtle(90), "w")
+turtle.onkey(move_turtle(270), "s")
+turtle.onkey(move_turtle(0), "d")
+turtle.onkey(move_turtle(180), "a")
+turtle.listen()
+
+turtle.exitonclick()
